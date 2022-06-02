c08b8964ba5f767f19cbd933238333dbf8e84bab
jojobyte <184880+jojobyte@users.noreply.github.com>
2022-06-02 10:25:59 -0600

    Cleanup code

--- bin/traefik-jam.js
+++ bin/traefik-jam.js
@@ -8,8 +8,6 @@ import pkg from '../package.json' assert { type: 'json' }
 const { name, description, version } = pkg
 
 if (['-h','--help'].includes(process.argv[2])) {
-  // console.info(`\x1b[91mtraefik-jam\x1b[0m \x1b[92mv${pkg.version}\x1b[0m\n`)
-  // console.info(`\x1b[2m${'USAGE:'}\x1b[0m`)
   console.info(`
   \x1b[91m${name}\x1b[0m \x1b[92mv${version}\x1b[0m
     \x1b[2m${description.split('k L').join('k\n    L')}\x1b[0m
